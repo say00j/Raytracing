4c1e2b14fa8351c4d56c86478a107ced5b32c8a4
say00j <sayoojsayana123@gmail.com>
2025-11-18 19:42:36 +0530

    updated names and simplified code

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var divisionsPerSide = 10;
 
 var cellSize = scale / divisionsPerSide;
 
-var display_maze = document.getElementById("display_maze");
-display_maze.width = scale;
-display_maze.height = scale;
+var display = document.getElementById("display");
+display.width = scale;
+display.height = scale;
 
-var maze = display_maze.getContext("2d");
+var pen = display.getContext("2d");
 
-const mazeDesign = [
+const maze = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
@@ -29,41 +29,41 @@ var offset = playerSize / 2;
 var playerSpeed = 2;
 var mouseX = 0;
 var mouseY = 0;
+let dirY = 0;
+let dirX = 0;
 
 function drawDivision(x, y, width, height, color) {
-  maze.strokeStyle = "white";
-  maze.fillStyle = color;
-  maze.strokeRect(x, y, width, height);
-  maze.fillRect(x, y, width, height);
+  pen.strokeStyle = "white";
+  pen.fillStyle = color;
+  pen.strokeRect(x, y, width, height);
+  pen.fillRect(x, y, width, height);
 }
 
 function drawPlayer(x, y, width, height, color) {
-  maze.fillStyle = color;
-  maze.fillRect(x, y, width, height);
+  pen.fillStyle = color;
+  pen.fillRect(x, y, width, height);
 }
 
 function drawLine(x1, y1, x2, y2, color) {
-  maze.strokeStyle = color;
-  maze.beginPath();
-  maze.moveTo(x1, y1);
-  maze.lineTo(x2, y2);
-  maze.stroke();
+  pen.strokeStyle = color;
+  pen.beginPath();
+  pen.moveTo(x1, y1);
+  pen.lineTo(x2, y2);
+  pen.stroke();
 }
 
 window.addEventListener("keydown", (event) => {
-  console.log(event);
-  if (event.key === "ArrowUp" || event.key === "w") {
-    playerY -= playerSpeed;
-  }
-  if (event.key === "ArrowDown" || event.key === "s") {
-    playerY += playerSpeed;
-  }
-  if (event.key === "ArrowLeft" || event.key === "a") {
-    playerX -= playerSpeed;
-  }
-  if (event.key === "ArrowRight" || event.key === "d") {
-    playerX += playerSpeed;
-  }
+  if (event.key === "ArrowUp" || event.key === "w") dirY = -1;
+  if (event.key === "ArrowDown" || event.key === "s") dirY = 1;
+  if (event.key === "ArrowLeft" || event.key === "a") dirX = -1;
+  if (event.key === "ArrowRight" || event.key === "d") dirX = 1;
+});
+
+window.addEventListener("keyup", (event) => {
+  if (event.key === "ArrowUp" || event.key === "w") if (dirY === -1) dirY = 0;
+  if (event.key === "ArrowDown" || event.key === "s") if (dirY === 1) dirY = 0;
+  if (event.key === "ArrowLeft" || event.key === "a") if (dirX === -1) dirX = 0;
+  if (event.key === "ArrowRight" || event.key === "d") if (dirX === 1) dirX = 0;
 });
 
 window.addEventListener("mousemove", (event) => {
@@ -71,21 +71,30 @@ window.addEventListener("mousemove", (event) => {
   mouseY = event.clientY;
 });
 
-function animate() {
-  requestAnimationFrame(animate);
-  maze.clearRect(0, 0, display_maze.width, display_maze.height);
+function drawMaze() {
   for (let i = 0; i < divisionsPerSide; i++) {
     for (let j = 0; j < divisionsPerSide; j++) {
-      if (mazeDesign[i][j] == 1) {
+      if (maze[i][j] == 1) {
         drawDivision(j * cellSize, i * cellSize, cellSize, cellSize, "green");
       } else {
         drawDivision(j * cellSize, i * cellSize, cellSize, cellSize, "black");
       }
     }
   }
+}
 
+function playerUpdate() {
+  playerX += dirX * playerSpeed;
+  playerY += dirY * playerSpeed;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  pen.clearRect(0, 0, display.width, display.height);
+  drawMaze();
   drawPlayer(playerX, playerY, playerSize, playerSize, "yellow");
   drawLine(playerX + offset, playerY + offset, mouseX, mouseY, "cyan");
+  playerUpdate();
 }
 
 animate();
